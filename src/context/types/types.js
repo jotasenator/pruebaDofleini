1383700852a40c49218004a8dc3f6ccb0239d02e
jotas <jotasenator@gmail.com>
2021-04-21 19:10:51 -0400

    version final types

--- src/context/types/types.js
+++ src/context/types/types.js
@@ -1,14 +1,6 @@
 
 export const types = {
-    color: 'CHANGE_COLOR',
 
-    inputComponent: 'CHANGE_INPUT_SPACE',
-
-    inputSuffixComponent: 'CHANGE_INPUT_DOMINE',
-
-    personas: 'CHANGE_PERSONAS',
-
-    privacidad: 'CHANGE_PRIVACIDAD',
 
     globalTest: 'GLOBAL'
 
