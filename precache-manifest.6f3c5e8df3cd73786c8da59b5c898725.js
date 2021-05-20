312ce4ea5977907806920447424ddf582d145995
jotas <jotasenator@gmail.com>
2021-05-20 14:43:08 -0400

    Updates

---
+++ precache-manifest.6f3c5e8df3cd73786c8da59b5c898725.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "86af65ad14da0f31e1b8",
+    "url": "/pruebaDofleini/static/css/main.6b1ff8a8.chunk.css"
+  },
+  {
+    "revision": "86af65ad14da0f31e1b8",
+    "url": "/pruebaDofleini/static/js/main.152b643f.chunk.js"
+  },
+  {
+    "revision": "6b9b600bf4e8141f3d49",
+    "url": "/pruebaDofleini/static/js/runtime~main.cea1bdfd.js"
+  },
+  {
+    "revision": "c7a8e31b8680335ebeda",
+    "url": "/pruebaDofleini/static/css/2.d47e9f6f.chunk.css"
+  },
+  {
+    "revision": "c7a8e31b8680335ebeda",
+    "url": "/pruebaDofleini/static/js/2.686a9adc.chunk.js"
+  },
+  {
+    "revision": "512b0d41b2938b952423f1194ed8f675",
+    "url": "/pruebaDofleini/static/media/Group 2493.512b0d41.svg"
+  },
+  {
+    "revision": "797b0638696cf94c5595db0fd9ae3f7d",
+    "url": "/pruebaDofleini/static/media/fira-sans-cyrillic-ext-400-normal.797b0638.woff2"
+  },
+  {
+    "revision": "7dba4aa82320f74f9d216f40c85d0641",
+    "url": "/pruebaDofleini/static/media/fira-sans-cyrillic-400-normal.7dba4aa8.woff2"
+  },
+  {
+    "revision": "e298b229de09306d21b08733aab35c32",
+    "url": "/pruebaDofleini/static/media/fira-sans-greek-ext-400-normal.e298b229.woff2"
+  },
+  {
+    "revision": "783d115d0a492f5ddf091b21fb5f6552",
+    "url": "/pruebaDofleini/static/media/fira-sans-greek-400-normal.783d115d.woff2"
+  },
+  {
+    "revision": "22681e0a8a6eed96fd07c52ec1fe08fe",
+    "url": "/pruebaDofleini/static/media/fira-sans-all-400-normal.22681e0a.woff"
+  },
+  {
+    "revision": "3877857444204c553108aa039630e657",
+    "url": "/pruebaDofleini/static/media/fira-sans-vietnamese-400-normal.38778574.woff2"
+  },
+  {
+    "revision": "98410ba5217856a45ac970b605f4df28",
+    "url": "/pruebaDofleini/static/media/fira-sans-latin-400-normal.98410ba5.woff2"
+  },
+  {
+    "revision": "7c8a98460cc27b5977412d5c35b6fd20",
+    "url": "/pruebaDofleini/static/media/fira-sans-latin-ext-400-normal.7c8a9846.woff2"
+  },
+  {
+    "revision": "6664c6adbfd749c7d4ca9e5eccc108f9",
+    "url": "/pruebaDofleini/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pruebaDofleini/precache-manifest.6f3c5e8df3cd73786c8da59b5c898725.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/pruebaDofleini/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
